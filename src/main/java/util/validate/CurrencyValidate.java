c51fd34128f58524284d0700f4de9e0d659e4348
alim <asas@gmail.com>
2024-01-31 04:26:27 +0400

    refactored regular expressions for rate and id

--- src/main/java/util/validate/CurrencyValidate.java
+++ src/main/java/util/validate/CurrencyValidate.java
@@ -5,17 +5,16 @@ import java.util.regex.Pattern;
 public class CurrencyValidate {
 
     public static boolean validateId(int id) {
-        String regex = "^\\d+$";
+        String regex = "^[1-9]+$";
         Pattern pattern = Pattern.compile(regex);
         return pattern.matcher(String.valueOf(id).trim()).matches();
     }
 
     public static boolean validateRate(double rate) {
-        String regex = "^\\d+.\\d+$";
+        String regex = "^\\d*\\.\\d+$";
         Pattern pattern = Pattern.compile(regex);
         return pattern.matcher(String.valueOf(rate).trim()).matches();
     }
-
 
     public static boolean validateCode(String code) {
         String regex = "^[A-Z]+$";
